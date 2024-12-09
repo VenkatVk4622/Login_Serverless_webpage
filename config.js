c0c2d8d581ef4d988d341eb2fdc35968b7823cd7
VenkatVK <venkatramananvk46@gmail.com>
2024-12-09 10:52:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_04U6FpR7i', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: '4kuttdsv35rlhqrrvigqpj2ha7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
+    }
+};
